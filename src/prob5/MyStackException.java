2ff063d75b1a0b38ab0515247992eb28fa1bfeb9
Alex <urimaumdero@hotmail.com>
2017-08-04 20:46:02 +0900

    예외클래스 기본생성자 추가

--- src/prob5/MyStackException.java
+++ src/prob5/MyStackException.java
@@ -3,6 +3,10 @@ package prob5;
 public class MyStackException extends Exception {
 	private static final long serialVersionUID = 1L;
 
+	public MyStackException(){
+		super("MyStackException occurs");
+	}
+
 	public MyStackException(String expMsg){
 		super(expMsg);
 	}
